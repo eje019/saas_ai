a9d6e05b247a77f326f8944877e49b052ef4619c
Jp <emricndayake@gmail.com>
2025-11-10 19:38:52 +0100

    Ajout des autres pages

---
+++ client/src/pages/BlogTitles.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogTitles = () => {
+  return (
+    <div>
+      <h1>Totres de Blogs</h1>
+    </div>
+  );
+};
+
+export default BlogTitles;
---
+++ client/src/pages/GenerateImages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GenerateImages = () => {
+  return <div>Images générées</div>;
+};
+
+export default GenerateImages;
