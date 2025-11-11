36dd2990294c490791558533341bb018cff44b30
Jp <emricndayake@gmail.com>
2025-11-11 20:45:27 +0100

    Correction des bugs-routes finies (normalement)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import WriteArticle from "./pages/WriteArticle.jsx";
+import Layout from "./pages/Layout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import BlogTitles from "./pages/BlogTitles.jsx";
+import GenerateImages from "./pages/GenerateImages.jsx";
+import RemoveObject from "./pages/RemoveObject.jsx";
+import ReviewResume from "./pages/ReviewResume.jsx";
+import Community from "./pages/Community.jsx";
 
 const App = () => {
   return (
     <div>
       <Routes>
+        {/* Route racine */}
         <Route path="/" element={<Home />} />
+
+        {/* Routes avec layout */}
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-objects" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+
+        {/* Route 404 au cas ou */}
+        <Route path="*" element={<div>Page non trouvée</div>} />
       </Routes>
     </div>
   );
 };
+
 export default App;
