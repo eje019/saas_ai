a48c7307d7ab4e274525924c478afe0c32f2f13e
Jp <emricndayake@gmail.com>
2025-11-12 00:06:44 +0100

    Correctioon finale Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,19 +7,26 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
-      <img
-        src={assets.logo}
-        alt="logo"
-        className="w-32 sm:w-44"
-        onClick={() => navigate("/")}
-      />
-      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
+    <nav className="fixed top-0 left-0 w-full bg-white border-b border-gray-300 py-4 px-6 flex justify-between items-center z-50 shadow-lg">
+      {/* Logo */}
+      <div className="">
+        <img
+          src={assets.logo}
+          alt=""
+          className="w-32 sm:w-44"
+          onClick={() => navigate("/")}
+        ></img>
+      </div>
+
+      {/* Bouton */}
+      <button
+        className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        onClick={() => navigate("/ai")}
+      >
         Get Started
         <ArrowRight className="w-4 h-4" />
       </button>
-    </div>
+    </nav>
   );
 };
-
 export default Navbar;
