be9a2e374f9f560b13a987f096d815702c5ec73d
Jp <emricndayake@gmail.com>
2025-11-15 10:43:02 +0100

    Hero toujours...

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className="px-4 sm:px-20 xl:px-32 relative incline-flex flex-col w-full justify-center bg-[url//gradientBackground.png] bg-cover bg-no-repeat min-h-screen">
+        
+    </div>
+  );
 };
 
 export default Hero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
+      <Hero />
     </div>
   );
 };
